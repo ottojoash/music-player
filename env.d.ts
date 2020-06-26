1ed594f04c3515de9abfa2ae1a4108ab1719dd79
jeisonBorba <jeizon.borba@gmail.com>
2020-06-26 02:10:55 -0300

    setting up environment variables

---
+++ env.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-native-dotenv' {
+  export const SPOTIFY_TOKEN_URL: string;
+  export const SPOTIFY_API_URL: string;
+  export const CLIENT_ID: string;
+  export const CLIENT_SECRET: string;
+}
